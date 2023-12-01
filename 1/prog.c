4760f12b9d5285b2a05a04cbd3192cd91e635df6
Isaiah Deppong <isaiahjdeppong@gmail.com>
2023-12-01 12:35:52 -0500

    changed default c file. finished day 1! we love a good sub string problem in c :)

--- 1/prog.c
+++ 1/prog.c
@@ -1,29 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-/*
-    A X -> ROCK     1
-    B Y -> PAPER    2
-    C Z -> SCISSORS 3
-
-    p1 A B C
-  p2X  3 0 6
-    Y  6 3 0
-    Z  0 6 3
-    
-*/
+#include <ctype.h>
 
 int main(void) {
-    char ai, me;
+    char str[1024];
+    char *map[] = {
+        "zero","one","two","three","four","five","six", "seven","eight","nine"
+    };
+    char *s;
     int sum = 0;
-    int key[] = {3,0,6,6,3,0,0,6,3};
-    while(scanf("%c %c\n", &ai, &me) > 0) {
-        ai-=65;me-=88;
-        printf("%d %d -> %d\n", ai, me, key[ai+me*3]);
-        sum+=me+1+key[ai+me*3];
+    while(scanf("%s\n", str) > 0) {
+        // Part 2:
+        for(int i=0;i<10;i++) {
+            while((s = strstr(str, map[i])) != NULL) {
+                memset(s+1, i+48, 1);
+            }
+        }
+        printf("%s\n", str);
+        // --------
+        char first, last;
+        int is_first = 1;
+        for (int i = 0; i < strlen(str); i++) {
+            if (isdigit(str[i])) {
+                if (is_first) {
+                    first = str[i];
+                    last = str[i];
+                    is_first=0;
+                } else {
+                    last = str[i];
+                }
+            }
+        }
+        char outbuf[1024];
+        sprintf(outbuf, "%c%c", first, last);
+        printf("%s\n",outbuf);
+        sum+=atoi(outbuf);
     } 
-    printf("%d\n", sum);
+    printf("%d\n",sum);
 
     return 0;
 }
--- 10/prog.c
+++ 10/prog.c
@@ -1,27 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-/*
-    A X -> ROCK     1
-    B Y -> PAPER    2
-    C Z -> SCISSORS 3
-
-    p1 A B C
-  p2X  3 0 6
-    Y  6 3 0
-    Z  0 6 3
-    
-*/
+void part1() {}
 
 int main(void) {
-    char ai, me;
+    char str[1024];
     int sum = 0;
-    int key[] = {3,0,6,6,3,0,0,6,3};
-    while(scanf("%c %c\n", &ai, &me) > 0) {
-        ai-=65;me-=88;
-        printf("%d %d -> %d\n", ai, me, key[ai+me*3]);
-        sum+=me+1+key[ai+me*3];
+    while(scanf("%s\n", str) > 0) {
+        printf("%s\n",str);
+        sum++;
     } 
     printf("%d\n", sum);
 
