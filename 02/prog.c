2d7e4fd944ea915cde64cda1d4d28584d276f36d
Isaiah Deppong <isaiahjdeppong@gmail.com>
2023-12-02 01:02:18 -0500

    part1 of day2 done, will try and finish later

--- 02/prog.c
+++ 02/prog.c
@@ -8,9 +8,27 @@ void part1() {}
 int main(void) {
     char str[1024];
     int sum = 0;
-    while(scanf("%s\n", str) > 0) {
-        printf("%s\n",str);
-        sum++;
+    int game = 1;
+    while(fgets(str, sizeof str, stdin) != NULL) {
+        int prev_digit = 0;
+        char *p;
+        int flag = 0;
+        printf("str: %s\n", str);
+        for(p = strtok(str, " :,;\n"); p!=NULL; p=strtok(NULL, " :,;\n")) {
+            if (isdigit(p[0])) {
+                prev_digit=atoi(p);
+            } else {
+                switch(p[0]) {
+                    case 'r': if(prev_digit>12) {flag=1;}; break;
+                    case 'g': if(prev_digit>13) {flag=1;}; break;
+                    case 'b': if(prev_digit>14) {flag=1;}; break;
+                }
+            }
+            if (flag) break;
+        }
+
+        if (!flag) { sum += game; printf("possible, id: %d\n", game); }
+        game++;
     } 
     printf("%d\n", sum);
 
